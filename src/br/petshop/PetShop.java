6dc29c33468192c0be4fad5b23cfee53c0bf58da
Ricardo <ricarducorrea@gmail.com>
2020-08-18 19:57:34 -0300

    Add files via upload

---
+++ src/br/petshop/PetShop.java
@@ -0,0 +1,10 @@
+package br.petshop;
+
+public class PetShop {
+	private String nome;
+	private String endereco;
+	private String cnpj;
+	private String razaoSocial;
+	private String nomePropietario;
+
+}
